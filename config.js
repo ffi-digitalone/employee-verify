2e333139a5081faff681d8546c93bf3b574aed2e
ffi-digitalone <ffi.digitalone@gmail.com>
2026-07-30 03:26:36 +0600

    Add Google Sheet configuration in config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Google Sheet Configuration
+
+const CONFIG = {
+
+  // Google Sheet Published CSV URL
+  SHEET_URL: "",
+
+  // Company Information
+  COMPANY_NAME: "Digital One Broadband Internet Service",
+
+  // Verification Title
+  TITLE: "Employee Verification"
+
+};
