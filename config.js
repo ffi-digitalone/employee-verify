708390f8d7d9ef29362038283a4d4bb0ae56abea
ffi-digitalone <ffi.digitalone@gmail.com>
2026-07-30 08:27:35 +0600

    Update SHEET_URL and add DEFAULT_PHOTO to config

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const CONFIG = {
-  SHEET_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSkUH5ZQOVC1hohn2J4B8lhZDCVKS996eTdYiFP1JzTZ7_my9AGfTi8cy9oUm0kb4yXbjLZ6D8njvJ1/pub?output=csv",
 
-  COMPANY_NAME: "Digital One Broadband Internet Service",
+SHEET_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSkUH5ZQOVC1hohn2J4B8lhZDCVKS996eTdYiFP1JzTZ7_my9AGfTi8cy9oUm0kb4yXbjLZ6D8njvJ1/pub?gid=846585429&single=true&output=csv",
 
-  LOGO_URL: "https://raw.githubusercontent.com/ffi-digitalone/employee-verify/main/assets/logo.png"
+COMPANY_NAME: "Digital One Broadband Internet Service",
+
+LOGO_URL: "https://raw.githubusercontent.com/ffi-digitalone/employee-verify/main/assets/logo.png",
+
+DEFAULT_PHOTO: "https://raw.githubusercontent.com/ffi-digitalone/employee-verify/main/assets/user.png"
+
 };
